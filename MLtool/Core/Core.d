e3ea29f49ea17682ccbb83ae57ec95a9b609ab54
septicmk <septic.mk@gmail.com>
2015-11-15 22:22:07 +0800

    DT

--- MLtool/Core/Core.d
+++
@@ -1,1 +0,0 @@
-Core.o: Core/Core.cpp Core/../MLalgorithm/MLalgorithm.h Core/Core.h
--- MLtool/Kmeans/Kmeans.d
+++
@@ -1,2 +0,0 @@
-Kmeans.o: Kmeans/Kmeans.cpp Kmeans/Kmeans.h \
- Kmeans/../MLalgorithm/MLalgorithm.h Kmeans/../Core/Core.h
